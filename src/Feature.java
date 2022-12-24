466662aefe48fe8060d4ccc03abf704db73f48ba
aranw <aranweizman@gmail.com>
2022-12-24 21:14:29 +0200

    added feature description functionality to class Feature

--- src/Feature.java
+++ src/Feature.java
@@ -1,5 +1,11 @@
 public class Feature {
-    public Feature(){
+    private final String feature;
+    public Feature(String feature){
         System.out.println("This is the feature");
+        this.feature = feature;
+    }
+
+    public String getFeature(){
+        return feature;
     }
 }
