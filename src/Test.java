03df0b4ab6d3eabca0d22a29e866a2b5c1c7047f
aranw <aranweizman@gmail.com>
2022-12-31 18:43:37 +0200

    readded Test class

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    Test(){
+        System.out.println("This is the test");
+    }
+
+    public boolean getResult(){
+        System.out.println("you failed Elias test");
+        return false;
+    }
+}
