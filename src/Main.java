77233fad3fad300e48549896604d79d8aa9f95b8
aranw <aranweizman@gmail.com>
2022-12-31 18:31:08 +0200

    made modulations

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main{
     public static void main(String[] args) {
         newFeature flag = new newFeature();
         Test test = new Test();
+
+        System.out.println("just made more modulation to main");
     }
 }
